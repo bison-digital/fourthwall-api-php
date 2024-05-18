6670a42519129a3c7d204e8e41b568adc912b65b
Zach Bimson <zach.bimson@gmail.com>
2024-05-18 20:11:25 +0100

    Add default for validators array

--- src/Validator/WebhookRequestValidator.php
+++ src/Validator/WebhookRequestValidator.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\RequestInterface;
 
 final class WebhookRequestValidator {
     public function __construct(
-        private array $validators,
+        private array $validators = [],
     ) {
     }
 
